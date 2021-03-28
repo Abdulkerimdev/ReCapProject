567c82792e83b3990a6ab3cc44107ab6adefb6fd
abdul <abdul@DESKTOP-SI43Q1K>
2021-03-28 17:16:19 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            carManager.Add(new Car
+            {
+                Id = 5,
+                BrandId = 4,
+                ColorId = 6,
+                DailyPrice = 4000,
+                Description = "garaj otosu",
+                ModelYear = 1999
+            });
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.ModelYear);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> cars;
+        public InMemoryCarDal()
+        {
+            cars = new List<Car> { new Car { Id=1, BrandId=1, ColorId=1, DailyPrice= 100,
+                Description="Hoş araba", ModelYear= 2010},
+                new Car { Id=2 ,BrandId=2, ColorId=2, DailyPrice = 250,
+                    Description= "Müko araba", ModelYear = 2015} };
+        }
+        public void Add(Car car)
+        {
+            cars.Add(car);
+        }
+
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = cars.SingleOrDefault(c => c.Id == car.Id);
+            cars.Remove(carToDelete);
+        }
+
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.ModelYear = car.ModelYear;
+
+        }
+
+        List<Car> ICarDal.GetAll()
+        {
+            return cars;
+        }
+
+        List<Car> ICarDal.GetById(int Id)
+        {
+            return cars.Where(c => c.Id == Id).ToList();
+        }
+    }
+}
